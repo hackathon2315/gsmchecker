795b2497232627385ea333d91427cb520156055a
Alameddin Çelik <alameddinc@gmail.com>
2023-05-03 22:08:25 +0300

    Updated as Cell Signal

--- app/src/main/java/com/gsm/gsmchecker/MainActivity.kt
+++ app/src/main/java/com/gsm/gsmchecker/MainActivity.kt
@@ -4,7 +4,12 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.telephony.CellInfoGsm
+import android.telephony.CellInfoLte
+import android.telephony.CellSignalStrengthGsm
+import android.telephony.CellSignalStrengthLte
 import android.telephony.TelephonyManager
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -13,7 +18,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val permission = Manifest.permission.READ_PHONE_STATE
+    private val permissionReadPhoneState = Manifest.permission.READ_PHONE_STATE
+    private val permissionFineLocation = Manifest.permission.ACCESS_FINE_LOCATION
     private val permissionRequestCode = 1
     private lateinit var timer: Timer
 
@@ -21,41 +27,71 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TelephonyManager izni kontrolü
-        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-            // İzin verilmedi, izin isteği yapılıyor
-            ActivityCompat.requestPermissions(this, arrayOf(permission), permissionRequestCode)
+        // İzinlerin kontrolü yapılır, eğer izin yoksa kullanıcıdan istenir
+        if (ContextCompat.checkSelfPermission(this, permissionReadPhoneState) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(this, permissionFineLocation) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(permissionReadPhoneState, permissionFineLocation), permissionRequestCode)
         } else {
-            // İzin verilmiş, GSM bağlantı kontrolü yapılıyor
-            checkGsmConnection()
+            // İzinler verildiyse GSM bağlantı kontrolü yapılır
+            startTimer()
         }
     }
 
     private fun checkGsmConnection() {
+        Log.i("GSM", "checkGsmConnection")
+
+        // İzinler kontrol edilir, eğer izin yoksa checkGsmConnection() yöntemi sonlandırılır
+        if (ContextCompat.checkSelfPermission(this, permissionFineLocation) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("GSM", "checkGsmConnection -1")
+            return
+        }
+
         // TelephonyManager kullanarak GSM bağlantı kontrolü yapılır
         val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        if (telephonyManager.simState == TelephonyManager.SIM_STATE_READY) {
-            // GSM bağlantısı var
-            Toast.makeText(this, "GSM bağlantısı var", Toast.LENGTH_SHORT).show()
+        val cellInfoList = telephonyManager.allCellInfo
+
+        if (cellInfoList != null) {
+            var gsmSignalFound = false
+            var signalStrengthDbm = 0
+            var typeOfGSM = ""
+            for (cellInfo in cellInfoList) {
+                Log.i("GSM", cellInfo.toString())
+                if (cellInfo is CellInfoLte) {
+                    val cellSignalStrengthLte = cellInfo.cellSignalStrength as CellSignalStrengthLte
+                    signalStrengthDbm = cellSignalStrengthLte.dbm
+                    gsmSignalFound = true
+                    typeOfGSM = "LTE"
+                    break
+                } else if (cellInfo is CellInfoGsm) {
+                    val cellSignalStrengthGsm = cellInfo.cellSignalStrength as CellSignalStrengthGsm
+                    signalStrengthDbm = cellSignalStrengthGsm.dbm
+                    gsmSignalFound = true
+                    typeOfGSM = "GSM"
+                    break
+                }            }
+            if (!gsmSignalFound) {
+                // GSM sinyali yoksa kullanıcıya uygun bir mesaj gösterilir
+                Toast.makeText(this, "GSM signal not found", Toast.LENGTH_SHORT).show()
+                return
+            }
+            Toast.makeText(this, "GSM signal found: $signalStrengthDbm dBm ($typeOfGSM)", Toast.LENGTH_SHORT).show()
         } else {
-            // GSM bağlantısı yok
-            Toast.makeText(this, "GSM bağlantısı yok", Toast.LENGTH_SHORT).show()
+            // GSM sinyali yoksa kullanıcıya uygun bir mesaj gösterilir
+            Toast.makeText(this, "GSM signal not found", Toast.LENGTH_SHORT).show()
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == permissionRequestCode) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // İzin verildi, GSM bağlantı kontrolü yapılıyor
-                checkGsmConnection()
 
-                // Timer'ı başlat
-                startTimer()
-            } else {
-                // İzin verilmedi
-                Toast.makeText(this, "TelephonyManager izni verilmedi", Toast.LENGTH_SHORT).show()
-            }
+        // İzinler kontrol edilir, verildiyse GSM bağlantı kontrolü yapılır
+        if (requestCode == permissionRequestCode && grantResults.isNotEmpty()
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            startTimer()
+        } else {
+            // İzin verilmedi
+            Toast.makeText(this, "TelephonyManager permission not granted", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -65,7 +101,6 @@ class MainActivity : AppCompatActivity() {
             override fun run() {
                 runOnUiThread {
                     checkGsmConnection()
-                    Toast.makeText(this@MainActivity, "GSM Connnection Checked", Toast.LENGTH_SHORT).show()
                 }
             }
         }, 0, 6000)
@@ -73,7 +108,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        // Uygulama kapatıldığında Timer'ı durdur
         timer.cancel()
     }
 }
